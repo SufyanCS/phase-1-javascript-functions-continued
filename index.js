e73738b4efd799bc257f3550079f34994ba73dfa
Sufyan Hussein <sufianb.contact@gmail.com>
2023-01-01 14:18:16 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(name ='roller-skate') {
+    return `This Saturday, I want to ${name}!`;
+}
+saturdayFun();
+function mondayWork(workType ="go to the office") {
+    return `This Monday, I will ${workType}.`
+}
+function wrapAdjective(par = "*") {
+    return function(name = "special"){
+        return `You are ${par}${name}${par}!`
+    }
+}
